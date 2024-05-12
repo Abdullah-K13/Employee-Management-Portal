132c013b5a21ea9d8f76b86aab9913e2ded01eea
Abdullah-K13 <abdullahk4803@gmail.com>
2024-05-12 14:31:20 +0500

    My first commit

---
+++ EmployeeComplaintPortal/src/main/java/Mainmethod.java
@@ -0,0 +1,28 @@
+
+
+import com.mycompany.logininterface.Interface;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class Mainmethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            Interface im = new Interface();
+     im.setVisible(true);
+
+// System.out.println("Hello World!");
+    }
+
+    
+}
